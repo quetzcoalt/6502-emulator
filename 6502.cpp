e4bcb0bc92eec94d537ef1273177f727b15ccbdd
quetzcoalt <quetzcoalt19@gmail.com>
2026-07-19 12:49:54 +0100

    execute

--- 6502.cpp
+++ 6502.cpp
@@ -28,6 +28,16 @@ struct Memory
             Data[i] = 0;
         }
     }
+
+    /* read 1 byte */
+    Byte operator[](u32 address) const {
+        return Data[address];
+    }
+
+    /* write 1 byte */
+    Byte& operator[](u32 address) {
+        return Data[address];
+    }
 };
 
 struct CPU
@@ -35,7 +45,7 @@ struct CPU
     // Program Counter
     Word PC;
 
-    // Registers: Accumulator, Stack POinter, Index Registers
+    // Registers: Accumulator, Stack Pointer, Index Registers
     Byte A, S, X, Y;
 
     // Status flag
@@ -56,6 +66,40 @@ struct CPU
 
         memory.Initialize();
     }
+
+    void Execute(u32 cycles, Memory& memory)
+    {
+        while (cycles > 0) {
+            Byte instruction = FetchByte(cycles, memory);
+            switch (instruction)
+            {
+                case INS_LDA_IM:
+                {
+                    Byte value = FetchByte(cycles, memory);
+                    A = value;
+
+                    Z = (A == 0);
+                    N = (A & 0b10000000) > 0;
+                } break;
+                default:
+                {
+                    printf("Instruction not handled %d", instruction);
+                } break;
+            }
+        }
+    }
+
+    Byte FetchByte(u32& cycles, Memory &memory) {
+        Byte data = memory[PC];
+        PC++;
+        cycles--;
+
+        return data;
+    }
+
+    // opcodes
+    static constexpr Byte
+        INS_LDA_IM = 0xA9;
 };
 
 
@@ -64,6 +108,13 @@ int main()
     Memory memory;
     CPU cpu;
     cpu.Reset(memory);
+
+    memory[0xFFFC] = CPU::INS_LDA_IM;
+    memory[0xFFFD] = 0x42;
+
+    cpu.Execute(2, memory);
+
+    cout << memory[0xFFFD] << endl;
 
     return 0;
 }
